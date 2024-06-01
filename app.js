78d190db8ab608c4dbc646ed4ebe4db1adcaa1ea
Surbhi Agarwal <151505533+push-gmail@users.noreply.github.com>
2024-06-01 15:40:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+let icons = document.getElementById("link1");
+icons.addEventListener("click", () => {
+    let anchor = document.getElementsByTagName("a");
+    anchor.href = "https://github.com/push-gmail/Stopwatch-Application";
+});
